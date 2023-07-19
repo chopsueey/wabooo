8e3050bf8426ac48912ba31c704b793015d81747
SoraSen <alexandra.popiel@dci.education>
2023-07-19 13:13:46 +0200

    styling&fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { userRegister, userLogin } from "../fetchRequests/UserRequests.jsx";
-import bg from "../assets/akin-cakiner-unsplash.jpg";
-// import bg2 from "../assets/OIG.LoWKTxfaR4d77t.jpeg"
+import bg from "../assets/background.jpg";
+import bg2 from "../assets/karte.jpg";
+import bg3 from "../assets/karte2.jpg";
+import bg4 from "../assets/karte3.jpg";
 import GeneralStore from "../store/GeneralContext";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
   const navigate = useNavigate();
   const { modal, setModal, hasCookie, setHasCookie } = GeneralStore();
   const [loading, setLoading] = useState(false);
-
   const [register, setRegister] = useState(false);
   const [name, setName] = useState("");
   const [userName, setUserName] = useState("");
@@ -52,7 +54,7 @@ export default function Home() {
   });
 
   return (
-    <div className="wrapper" style={{ height: "100vh" }}>
+    <div className="wrapper" style={{ height: "115vh" }}>
       <div
         className="flex flex-col justify-center"
         style={{
@@ -63,10 +65,26 @@ export default function Home() {
           height: "100%",
         }}
       >
-        <h1 className="text-4xl textc">Welcome to Wabooo!</h1>
+        {/* <h1 className="text-4xl textc">Welcome to Wabooo!</h1> */}
+
+        {/* text effekt */}
+        <div className="flex flex-col items-center justify-center h-2/4">
+          <h1 className="text-center pb-10  text-white text-5xl">
+            <Typewriter
+              options={{
+                strings: [
+                  "How many other people think like you?",
+                  "Sign up and find out!",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
+        </div>
 
         {modal && (
-          <div className="modal">
+          <div className="modal sm:mb-8">
             <form className="signin flex flex-col p-4 text-center bg-gray-800 rounded-lg max-w-md mx-auto">
               <span
                 className="textc font-bold"
@@ -182,6 +200,68 @@ export default function Home() {
             </form>
           </div>
         )}
+        {/* karten */}
+        <div
+          className="flex flex-wrap justify-center gap-4 sm:mb-8"
+          style={{ marginBottom: "5rem" }}
+        >
+          {/* Card eins */}
+          <div className="max-w-xs mx-4 blubb shadow-md rounded-lg overflow-hidden">
+            <div
+              className="w-full h-32 object-cover"
+              style={{
+                backgroundImage: `url(${bg2})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+              }}
+              // src="https://dummyimage.com/150%20x%20150/1f2937/06b5d4.jpg&text=+Wabooo"
+            />
+            <div className="py-4 px-6">
+              <h2 className="text-cyan-300 text-xl font-semibold">
+                You have a question?
+              </h2>
+              <p className="mt-2 text-cyan-500">Ask Wabooo!</p>
+            </div>
+          </div>
+
+          {/* Card zwei */}
+          <div className="max-w-xs mx-4 blubb shadow-md rounded-lg overflow-hidden">
+            <div
+              className="w-full h-32 object-cover"
+              style={{
+                backgroundImage: `url(${bg3})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+              }}
+              // src="https://dummyimage.com/150%20x%20150/1f2937/06b5d4.jpg&text=+Wabooo"
+            />
+            <div className="py-4 px-6">
+              <h2 className="text-cyan-300 text-xl font-semibold">
+                You have a question?
+              </h2>
+              <p className="mt-2 text-cyan-500">Ask Wabooo!</p>
+            </div>
+          </div>
+
+          {/* Card drei */}
+          <div className="max-w-xs mx-4 blubb shadow-md rounded-lg overflow-hidden">
+            <div
+              className="w-full h-32 object-cover"
+              style={{
+                backgroundImage: `url(${bg4})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+              }}
+              // src="https://dummyimage.com/150%20x%20150/1f2937/06b5d4.jpg&text=+Wabooo"
+            />
+            <div className="py-4 px-6">
+              <h2 className="text-cyan-300 text-xl font-semibold">
+                You have a question?
+              </h2>
+              <p className="mt-2 text-cyan-500">Ask Wabooo!</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
