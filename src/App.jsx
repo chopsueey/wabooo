ba6401452c5fa8d4879565edc9d7e7ceb50b9f0d
chopsueey <meyer-marius@live.de>
2023-10-16 12:40:28 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -15,60 +15,61 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const navigate = useNavigate();
-  // cookie check for sensitive websitedata?
-  const { hasCookie, setHasCookie } = GeneralStore();
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    document.cookie.includes("isLoggedIn")
-      ? (() => {
-          setHasCookie(true);
-          setIsLoading(false);
-        })()
-      : (() => {
-          setIsLoading(false);
-        })();
-    console.log("Logged in?", hasCookie);
-  }, [hasCookie]);
+    const navigate = useNavigate();
+    // cookie check for sensitive websitedata?
+    const { hasCookie, setHasCookie } = GeneralStore();
+    // const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        console.log("Logged in?", hasCookie);
 
-  return (
-    <div>
-        <Routes>
-          <Route path="/" element={<Navigation />}>
-            <Route index element={<Home />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            {/* <Route path="dashboard/search" element={<Dashboard />} /> */}
-            <Route
-              path="dashboard/user/profile/:profileId"
-              element={<Profile />}
+        if (document.cookie.includes("isLoggedIn")) {
+            setHasCookie(true);
+        }
+
+        console.log("Logged in?", hasCookie);
+    }, []);
+
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Navigation />}>
+                    <Route index element={<Home />} />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    {/* <Route path="dashboard/search" element={<Dashboard />} /> */}
+                    <Route
+                        path="dashboard/user/profile/:profileId"
+                        element={<Profile />}
+                    />
+                    <Route
+                        path="dashboard/:userName/profile/:profileId"
+                        element={<OthersProfile />}
+                    />
+                    <Route
+                        path="dashboard/question/:userName/:questionId"
+                        element={<QuestionPage />}
+                    />
+                    <Route
+                        path="dashboard/myquestions"
+                        element={<MyQuestions />}
+                    />
+                    <Route path="/logout" element={<Logout />} />
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
             />
-            <Route
-              path="dashboard/:userName/profile/:profileId"
-              element={<OthersProfile />}
-            />
-            <Route
-              path="dashboard/question/:userName/:questionId"
-              element={<QuestionPage />}
-            />
-            <Route path="dashboard/myquestions" element={<MyQuestions />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
