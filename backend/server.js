dc1a053bc1086b345250a748f397396d562b881d
chopsueey <meyer-marius@live.de>
2024-08-23 15:47:22 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,26 @@ import notFoundMiddleware from "./middleware/notFoundMiddleware.js";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authMiddleware } from "./middleware/authMiddleware.js";
 import statisticsRouter from "./routes/statisticsRouter.js";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(import.meta.url);
+
+const __filename = path.dirname(__dirname);
+
 const app = express();
 
 const port = process.env.PORT || 5050;
 const connectionString = process.env.MONGO_URL;
 
+app.use(express.static(path.join(__dirname, "../../dist")));
+
+
+
 // Start MIDDLEWARES
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+// app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true }));
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(
@@ -27,7 +40,7 @@ app.use(
 );
 app.use("/", userRouter);
 
-app.use("/statistics", statisticsRouter)
+app.use("/statistics", statisticsRouter);
 app.use("/dashboard", authMiddleware, dashboardRouter);
 
 // Operational error handling
@@ -55,6 +68,10 @@ app.use("/dashboard", authMiddleware, dashboardRouter);
     console.log(error);
   }
 })();
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../dist", "index.html"));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
